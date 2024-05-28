c4244b0a6211a40469f713c3b1f585587c33a0ae
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-28 06:55:18 +0400

    From Abdallah

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,19 +1,9 @@
 export default function createIteratorObject(report) {
-  const employees = Object.values(report.allEmployees).flat();
-  let index = 0;
-
-  return {
-    next: () => {
-      if (index < employees.length) {
-        const value = employees[index];
-        index += 1;
-        return { value, done: false };
-      } else {
-        return { value: undefined, done: true };
-      }
-    },
-    [Symbol.iterator]: function () {
-      return this;
-    },
-  };
+  const iterator = [];
+  for (const k in report.allEmployees) {
+    if (report.allEmployees[k]) {
+      iterator.push(...report.allEmployees[k]);
+    }
+  }
+  return iterator;
 }
