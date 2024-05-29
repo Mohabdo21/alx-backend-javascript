3af81e79565d02a9ce874c2691ce7c0958ff792f
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-29 08:03:07 +0400

    fix typos

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 function uploadPhoto(fileName) {
-  return Promise.reject(new Error(`${fileName} cannot bo proccessed`));
+  return Promise.reject(new Error(`${fileName} cannot be processed`));
 }
 
 export default uploadPhoto;
