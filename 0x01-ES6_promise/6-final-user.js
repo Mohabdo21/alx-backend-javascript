82376bf80586f2c56d2849287e1da50e73eac27b
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-29 09:37:34 +0400

    4th try

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName,
-) {
-  try {
-    const user = await signUpUser(firstName, lastName);
-
-    await uploadPhoto(fileName);
-
-    return [
-      { status: 'fulfilled', value: user },
-      { status: 'fulfilled', value: `Photo ${fileName} uploaded successfully` },
-    ];
-  } catch (error) {
-    return [{ status: 'rejected', value: error }];
-  }
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value: result.reason || result.value,
+    })),
+  );
 }
