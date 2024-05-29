c604b66b82d6ee18813d77729ed66acd505e89bd
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-29 21:29:08 +0400

    work around

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,32 +2,37 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 async function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
 
-  const [userResult, photoResult] = await Promise.allSettled([
-    userPromise,
-    photoPromise,
-  ]);
+    const [userResult, photoResult] = await Promise.allSettled([
+      userPromise,
+      photoPromise,
+    ]);
 
-  const resultArray = [
-    {
-      status: userResult.status,
-      value:
-        userResult.status === 'fulfilled'
-          ? userResult.value
-          : String(userResult.reason),
-    },
-    {
-      status: photoResult.status,
-      value:
-        photoResult.status === 'fulfilled'
-          ? photoResult.value
-          : String(photoResult.reason),
-    },
-  ];
+    const resultArray = [
+      {
+        status: userResult.status,
+        value:
+          userResult.status === 'fulfilled'
+            ? userResult.value
+            : userResult.reason.message,
+      },
+      {
+        status: photoResult.status,
+        value:
+          photoResult.status === 'fulfilled'
+            ? photoResult.value
+            : photoResult.reason.message,
+      },
+    ];
 
-  return resultArray;
+    return resultArray;
+  } catch (error) {
+    console.error('Error in handleProfileSignup:', error);
+    throw error;
+  }
 }
 
 export default handleProfileSignup;
