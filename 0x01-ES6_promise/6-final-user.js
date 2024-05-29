9616d52e75b9feb39cbdec1940d30036f2befcbb
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-29 21:51:30 +0400

    add documentation

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+/**
+ * Handles user profile signup by calling signUpUser and uploadPhoto functions.
+ * @param {string} firstName - First name of the user.
+ * @param {string} lastName - Last name of the user.
+ * @param {string} fileName - Name of the photo file.
+ * @returns {Promise<Array>} - Array containing status and value for each promise.
+ */
+
 async function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
