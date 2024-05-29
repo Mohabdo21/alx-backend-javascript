1e4c623831f1384956d3c4e2229aff4d493320fb
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-29 09:13:13 +0400

    try to fix task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,18 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName,
-) {
-  const results = await Promise.allSettled([
+function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
-
-  return results.map((result) => ({
+  ]).then((results) => results.map((result) => ({
     status: result.status,
     value: result.status === 'fulfilled' ? result.value : result.reason,
-  }));
+  })));
 }
+
+export default handleProfileSignup;
