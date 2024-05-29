70abf327fe52ebbd2620579dd9b95c1a1d685619
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-29 20:31:52 +0400

    6. Handle multiple promises 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,42 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.reason || result.value,
-    })))
-    .catch((err) => ({ status: 'rejected', value: err }));
+// export default function handleProfileSignup(firstName, lastName, fileName) {
+//   return Promise.allSettled([
+//     signUpUser(firstName, lastName),
+//     uploadPhoto(fileName),
+//   ])
+//     .then((results) => results.map((result) => ({
+//       status: result.status,
+//       value: result.reason || result.value,
+//     })))
+//     .catch((err) => ({ status: 'rejected', value: err }));
+// }
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  const userPromise = signUpUser(firstName, lastName)
+    .then((value) => ({
+      status: 'fulfilled',
+      value,
+    }))
+    .catch((error) => ({
+      status: 'rejected',
+      value: error,
+    }));
+
+  const photoPromise = uploadPhoto(fileName)
+    .then((value) => ({
+      status: 'fulfilled',
+      value,
+    }))
+    .catch((error) => ({
+      status: 'rejected',
+      value: error,
+    }));
+
+  return Promise.all([userPromise, photoPromise]);
 }
