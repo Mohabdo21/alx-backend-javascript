0ee411508aa86ad28ca2ea18ba9c53d7da78997f
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-29 20:19:23 +0400

    6. Handle multiple promises 5

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,8 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.reason || result.value,
-  })));
+  ])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.reason || result.value,
+    })))
+    .catch((err) => ({ status: 'rejected', value: err }));
 }
