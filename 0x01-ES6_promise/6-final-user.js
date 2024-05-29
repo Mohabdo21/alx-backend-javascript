add672a49cf9745a1a63dcfea3323183d99d8dc7
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-29 09:39:40 +0400

    try 5th

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,10 +5,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) =>
-    results.map((result) => ({
-      status: result.status,
-      value: result.reason || result.value,
-    })),
-  );
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.reason || result.value,
+  })));
 }
