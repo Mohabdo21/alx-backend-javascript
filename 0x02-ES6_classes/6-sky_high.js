201b03c5a008f376515382697c738baa020983c1
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-31 05:05:41 +0400

    6. Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,28 @@
+import Building from './5-building';
+
+/**
+ *
+ * @class
+ * @classdesc extends the Building class
+ */
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = newFloors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
