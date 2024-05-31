b0e2a5a669626be9d9e2eca3cc08f5904560b928
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-31 04:34:45 +0400

    5. A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * @class
+ * @classdesc Building Class
+ */
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor === Building) {
+      throw new TypeError('Cannot instantiate from Building directly');
+    }
+    if (this.evacuationWarningMessage === undefined) {
+      throw new TypeError(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
