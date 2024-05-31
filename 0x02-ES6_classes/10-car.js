c190e21df84ea6e4d14ee10ee8fa4963c4156782
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-31 05:48:49 +0400

    10. Vroom

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,27 @@
+const cloneCarSymbol = Symbol('cloneCar');
+
+/**
+ *
+ * @class
+ * @classdesc define a car class
+ */
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+
+    /* Define the Symbol property as non-enumerable */
+    Object.defineProperty(this, cloneCarSymbol, {
+      value: true,
+      enumerable: false,
+    });
+  }
+
+  cloneCar() {
+    if (this[cloneCarSymbol] === true) {
+      return new this.constructor();
+    }
+    return null;
+  }
+}
