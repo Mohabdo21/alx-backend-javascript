a71a1f47daf2774b8510a1a61968580d20bb0241
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-05-31 04:39:14 +0400

    set sqft

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -16,6 +16,13 @@ export default class Building {
     this._sqft = sqft;
   }
 
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = newSqft;
+  }
+
   get sqft() {
     return this._sqft;
   }
