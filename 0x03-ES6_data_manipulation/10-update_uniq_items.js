cab88767fdffe5b533f8fa8d08827bee430f785b
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-04 01:22:25 +0400

    10. More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+function updateUniqueItem(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+}
+
+export default updateUniqueItem;
