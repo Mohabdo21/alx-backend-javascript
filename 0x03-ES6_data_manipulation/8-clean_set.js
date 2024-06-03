ad663c8bd1645233e49a78641ea9d56a9e6cd2c5
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-04 03:59:00 +0400

    Fix issue with task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,13 +6,13 @@
  */
 function cleanSet(set, startString) {
   if (startString === '') return '';
-  let result = '';
+  const result = [];
   for (const item of set) {
     if (item.startsWith(startString)) {
-      result += `${item.slice(startString.length)}-`;
+      result.push(item.slice(startString.length));
     }
   }
-  return result.slice(0, -1);
+  return result.join('-');
 }
 
 export default cleanSet;
