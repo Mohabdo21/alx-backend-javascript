17d600acfaf6228fde6b271eac5b7d67bb4a792a
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-04 04:09:54 +0400

    i know, the linter error

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,13 +5,21 @@
  * @returns {string} A string of Set values that start with `startString`, separated by '-'.
  */
 function cleanSet(set, startString) {
-  if (!startString || typeof set !== 'object') return '';
+  if (
+    typeof startString !== 'string' ||
+    typeof set !== 'object' ||
+    startString.length === 0
+  ) {
+    return '';
+  }
+
   const result = [];
   for (const item of set) {
     if (item.startsWith(startString)) {
       result.push(item.slice(startString.length));
     }
   }
+
   return result.join('-');
 }
 
