33b66ec8acf878be7875e0897f3573da5656837e
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-24 05:10:53 +0400

    another way

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -23,16 +23,17 @@
 // rl.on('close', () => {
 //   console.log('This important software is now closing');
 // });
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
   if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
+    console.log(`Your name is: ${chunk.toString().trim()}`);
   }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
