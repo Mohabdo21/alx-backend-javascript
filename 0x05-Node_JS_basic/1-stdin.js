595544ec4180210cce380ae11c65848e0e97ac31
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-24 05:07:08 +0400

    1. Using Process stdin debug

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,25 +1,38 @@
-const readline = require('readline');
+// const readline = require('readline');
+//
+// // Interface for input and output
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+//
+// // Welcome message
+// console.log('Welcome to Holberton School, what is your name?');
+//
+// // Handle the line event
+// rl.on('line', (input) => {
+//   console.log(`Your name is: ${input}`);
+// });
+//
+// // Handle the end event on the input stream
+// process.stdin.on('end', () => {
+//   rl.close();
+// });
+//
+// // Handle the close event
+// rl.on('close', () => {
+//   console.log('This important software is now closing');
+// });
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Interface for input and output
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-// Welcome message
-console.log('Welcome to Holberton School, what is your name?');
-
-// Handle the line event
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-});
-
-// Handle the end event on the input stream
 process.stdin.on('end', () => {
-  rl.close();
-});
-
-// Handle the close event
-rl.on('close', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
