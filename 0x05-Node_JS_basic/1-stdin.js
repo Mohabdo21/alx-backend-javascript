444cf6613b1fea90026ae49e8fa088cbea60165e
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-24 05:18:03 +0400

    debug 4

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,29 +1,4 @@
-// const readline = require('readline');
-//
-// // Interface for input and output
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-//
-// // Welcome message
-// console.log('Welcome to Holberton School, what is your name?');
-//
-// // Handle the line event
-// rl.on('line', (input) => {
-//   console.log(`Your name is: ${input}`);
-// });
-//
-// // Handle the end event on the input stream
-// process.stdin.on('end', () => {
-//   rl.close();
-// });
-//
-// // Handle the close event
-// rl.on('close', () => {
-//   console.log('This important software is now closing');
-// });
-
+// Welcome the user
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
@@ -38,6 +13,6 @@ process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
 
-process.stdin.on('error', (err) => {
-  process.stderr.write(`An error occurred: ${err.message}\n`);
-});
+// process.stdin.on('error', (err) => {
+//   process.stderr.write(`An error occurred: ${err.message}\n`);
+// });
