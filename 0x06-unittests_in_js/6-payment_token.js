8700bd9a6d4caa4657bc76c461a9de66c7f3bd6a
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-25 08:35:53 +0400

    debug 02

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -5,8 +5,10 @@
  * @param {boolean} success - A boolean value representing the success of API request.
  * @returns {Promise} - A promise that resolves with a success message when the input is true.
  */
-const getPaymentTokenFromAPI = (success) => (success
-  ? Promise.resolve({ data: 'Seccessful response from the API' })
-  : Promise.resolve());
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, reject) => {
+  if (success) {
+    resolve({ data: 'Successful response from the API' });
+  }
+});
 
 module.exports = getPaymentTokenFromAPI;
